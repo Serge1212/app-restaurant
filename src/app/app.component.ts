6306e8398862e8b1556854eb8461c4ef6ae40cbd
Serge1212 <sanichace@gmail.com>
2024-11-24 00:33:41 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'app-restaurant';
