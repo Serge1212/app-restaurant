7b80d8abd8b2cf8d3b799d3a377e7160b5361db4
Serge1212 <sanichace@gmail.com>
2024-11-23 17:40:59 +0200

    init styles config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        maxHeight: {
+          '0': '0',
+          '1/4': '25%',
+          '1/2': '50%',
+          '3/4': '75%',
+          'full': '100%',
+         },
+        maxWidth: {
+          '10': '10rem',
+          '1/4': '25%',
+          '1/2': '50%',
+          '3/4': '75%',
+        },
+        transitionProperty:{
+          'width': 'width',
+          'height': 'height'
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
